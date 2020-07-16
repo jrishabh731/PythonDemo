365f627b1391991d875cf622a57abbfe64ff0934
Rishabh JAin <jrishabh731@gmail.com>
2020-07-17 01:53:28 +0530

    Logging sample with creating a custom logger with formatters, file handlers, exception stack trace and Stream Handler.

--- logging_sample/calculator.py
+++ logging_sample/calculator.py
@@ -4,12 +4,17 @@ from logging_sample.employee import Employee
 calc_logger = logging.getLogger(__name__)
 calc_logger.setLevel(logging.INFO)
 
+stream_logger = logging.StreamHandler()
+stream_formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+stream_logger.setFormatter(stream_formatter)
+
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
 
 file_handler = logging.FileHandler('calculator.log')
 file_handler.setFormatter(formatter)
 
 calc_logger.addHandler(file_handler)
+calc_logger.addHandler(stream_logger)
 
 
 def add_num(x, y):
@@ -28,8 +33,13 @@ def mul_num(x, y):
 
 
 def div_num(x, y):
-    res = x / y
-    calc_logger.info(f"Div: {x} / {y} = {res}")
+    try:
+        res = x / y
+        calc_logger.info(f"Div: {x} / {y} = {res}")
+    except ZeroDivisionError:
+        calc_logger.exception("ZeroDivision Error!")
+    else:
+        return res
 
 
 num1 = 10
@@ -38,4 +48,12 @@ add_num(num1, num2)
 sub_num(num1, num2)
 mul_num(num1, num2)
 div_num(num1, num2)
+
+num1 = 10
+num2 = 0
+add_num(num1, num2)
+sub_num(num1, num2)
+mul_num(num1, num2)
+div_num(num1, num2)
+
 
